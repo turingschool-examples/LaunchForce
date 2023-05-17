04d00042e242781ca8feafd320e60db76e01bfe0
jkimball54 <jeremyck54@gmail.com>
2023-05-17 14:44:56 -0600

    Added Spacecraft Class & Tests

---
+++ LaunchForce.Testing/SpacecraftTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchForce.Testing
+{
+    public class SpacecraftTests
+    {
+        [Fact]
+        public void Spacecraft_Constructor_InstantiatesSpacecraftObject()
+        {
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+
+            Assert.Equal("Daedalus", daedalus.Name);
+            Assert.Equal(400, daedalus.Fuel);
+        }
+    }
+}

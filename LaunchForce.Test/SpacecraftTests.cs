9d167a3d0573ab02c4a099249cd04365bc3d73ec
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-05-17 15:35:40 -0600

    Add Spacecraft class

---
+++ LaunchForce.Test/SpacecraftTests.cs
@@ -0,0 +1,45 @@
+﻿namespace LaunchForce.Test
+{
+    public class SpacecraftTests
+    {
+        [Fact]
+        public void Constructor_TakesTwoArguments()
+        {
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+
+            Assert.Equal("Daedalus", daedalus.Name);
+            Assert.Equal(400, daedalus.Fuel);
+            Assert.Equal(new List<Person>(), daedalus.Personnel);
+        }
+
+        [Fact]
+        public void AddPersonnel_UpdatesListOfPersonnel()
+        {
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+            Person kathy = new Person("Kathy Chan", 10);
+            Person megan = new Person("Megan McMahon", 2);
+
+            daedalus.AddPersonnel(megan);
+            daedalus.AddPersonnel(kathy);
+
+            var expected = new List<Person> { megan, kathy };
+
+            Assert.Equal(expected, daedalus.Personnel);
+        }
+
+        [Fact]
+        public void Roster_ReturnsNamesOfAllPersonnel()
+        {
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+            Person kathy = new Person("Kathy Chan", 10);
+            Person megan = new Person("Megan McMahon", 2);
+
+            daedalus.AddPersonnel(megan);
+            daedalus.AddPersonnel(kathy);
+
+            var expected = new List<string> { megan.Name, kathy.Name };
+
+            Assert.Equal(expected, daedalus.Roster());
+        }
+    }
+}

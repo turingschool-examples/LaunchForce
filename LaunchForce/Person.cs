091d098d79ebf7276d3062ef002354e2ea6604de
jcepriano <jamescepriano@gmail.com>
2023-05-17 13:26:06 -0700

    Person Class Created

---
+++ LaunchForce/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace LaunchForce
+{
+    public class Person
+    {
+        public string Name;
+        public int Experience;
+        public List<string> Specialties;
+
+        public Person(string name, int experience)
+        {
+            Name = name;
+            Experience = experience;
+            Specialties = new List<string>();
+        }
+
+        public void AddSpecialty(string specialty)
+        {
+            Specialties.Add(specialty);
+        }
+    }
+}

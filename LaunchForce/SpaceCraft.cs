528645524154b60b27292121c4905a835f288f54
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-05-17 15:03:26 -0600

    Added SpaceCraft Class

--- LaunchForce/SpaceCraft.cs
+++ LaunchForce/SpaceCraft.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace LaunchForce
 {
@@ -10,11 +11,55 @@ namespace LaunchForce
     {
         public string Name;
         public int Fuel;
-
+        public List<Person> Personnel;
         public SpaceCraft(string name, int fuel)
         {
             Name = name;
             Fuel = fuel;
+            Personnel = new List<Person>();
         }
+
+        public void AddPersonnel(Person person)
+        {
+            Personnel.Add(person);
+        }
+
+        public List<string> Roster()
+        {
+            List<string> roster = new List<string>();
+
+            foreach(var person in Personnel)
+            {
+                roster.Add(person.Name);
+            }
+
+            return roster;
+        }
+
+        public int TotalXP()
+        {
+            int TotalXP = 0;
+
+            foreach (var person in Personnel)
+            {
+                TotalXP += person.Experience;
+            }
+
+            return TotalXP;
+        }
+
+        public double averageXP()
+        {
+            double averageXP = 0;
+
+            foreach(var person in Personnel)
+            {
+                averageXP += person.Exoerience;
+            }
+            averageXP / Personnel.Count;
+
+            return averageXP;
+        }
+
     }
 }

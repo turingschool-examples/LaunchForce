b24aa7159d60919d13ca6611e58295bfc440cbc7
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-05-17 15:35:15 -0600

    Add Person Class

---
+++ LaunchForce/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchForce
+{
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Experience { get; private set; }
+        public List<string> Specialties { get; private set; }
+
+        public Person(string name, int experience)
+        {
+            Name = name;
+            Experience = experience;
+            Specialties = new List<string>();
+        }
+
+        public void AddSpecialty(string specialty)
+        {
+            Specialties.Add(specialty);
+        }
+    }
+}

34924bf0cd4c1a42bde766447b6c246d1c47691f
SGrinstead <seth.grinstead3299@gmail.com>
2023-05-17 13:52:55 -0700

    Added Personnel and methods to Spacecraft

--- LaunchForce.Tests/SpacecraftTests.cs
+++ LaunchForce.Tests/SpacecraftTests.cs
@@ -15,6 +15,60 @@ namespace LaunchForce.Tests
 
             Assert.Equal("Daedalus", daedalus.Name);
             Assert.Equal(400, daedalus.Fuel);
+            Assert.Equal(new List<Person>(), daedalus.Personnel);
+        }
+
+        [Fact]
+        public void Spacecraft_AddPerson_AddsPersonToPersonnel()
+        {
+            Person kathy = new Person("Kathy Chan", 10);
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+
+            daedalus.AddPerson(kathy);
+
+            Assert.Equal(daedalus.Personnel[0], kathy);
+        }
+
+        [Fact]
+        public void Spacecraft_Roster_ReturnsStringListOfPersonnelNames()
+        {
+            Person kathy = new Person("Kathy Chan", 10);
+            Person seth = new Person("Seth Grinstead", 15);
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+
+            daedalus.AddPerson(kathy);
+            daedalus.AddPerson(seth);
+
+            List<string> roster = daedalus.Roster();
+
+            Assert.Equal("Kathy Chan", roster[0]);
+            Assert.Equal("Seth Grinstead", roster[1]);
+        }
+
+        [Fact]
+        public void SpaceCraft_TotalExperience_ReturnsCombinedExperienceOfAllPersonnel()
+        {
+            Person kathy = new Person("Kathy Chan", 10);
+            Person seth = new Person("Seth Grinstead", 15);
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+
+            daedalus.AddPerson(kathy);
+            daedalus.AddPerson(seth);
+
+            Assert.Equal(25, daedalus.TotalExperience());
+        }
+
+        [Fact]
+        public void Spacecraft_AverageExperience_ReturnsAvgExperienceOfAllPersonnnel()
+        {
+            Person kathy = new Person("Kathy Chan", 10);
+            Person seth = new Person("Seth Grinstead", 15);
+            Spacecraft daedalus = new Spacecraft("Daedalus", 400);
+
+            daedalus.AddPerson(kathy);
+            daedalus.AddPerson(seth);
+
+            Assert.Equal(12.5, daedalus.AverageExperience());
         }
     }
 }
--- LaunchForce/Spacecraft.cs
+++ LaunchForce/Spacecraft.cs
@@ -10,11 +10,49 @@ namespace LaunchForce
     {
         public string Name;
         public int Fuel;
+        public List<Person> Personnel;
 
         public Spacecraft(string name, int fuel)
         {
             Name = name;
             Fuel = fuel;
+            Personnel = new List<Person>();
+        }
+
+        public void AddPerson(Person person)
+        {
+            Personnel.Add(person);
+        }
+
+        public List<string> Roster()
+        {
+            List<string> roster = new List<string>();
+            foreach(var person in Personnel)
+            {
+                roster.Add(person.Name);
+            }
+            return roster;
+        }
+
+        public int TotalExperience()
+        {
+            int exp = 0;
+            foreach(var person in Personnel)
+            {
+                exp += person.Experience;
+            }
+            return exp;
+        }
+
+        public double AverageExperience()
+        {
+            double exp = 0;
+            foreach (var person in Personnel)
+            {
+                exp += person.Experience;
+            }
+            exp = exp / Personnel.Count;
+            return exp;
         }
     }
 }

e36f26eedb1551c23f5782882b4f2a71ab5bdbe1
joe10111 <centeno.joe@gmail.com>
2023-05-17 13:51:16 -0700

    Added SpaceCraft Class

---
+++ LaunchForce/SpaceCraft.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchForce
+{
+    public class SpaceCraft
+    {
+        public string Name;
+        public int Fuel;
+        public List<Person> Personnel;
+        public SpaceCraft(string name, int fuel) 
+        {
+            Name = name;
+            Fuel = fuel;
+        }
+
+        public void AddPersonnel(Person personToAdd) 
+        {
+            Personnel.Add(personToAdd);
+        }
+
+        public List<string> Roster () 
+        {
+            List<string> tempList = new List<string>();
+
+            foreach (var person in Personnel)
+            {
+                tempList.Add(person.Name);
+            }
+
+            return tempList;
+        }
+
+        public int TotalExperience()
+        {
+            int totalExp = 0;
+
+            foreach (var person in Personnel)
+            {
+                totalExp += person.Experience;
+            }
+
+            return totalExp;
+        }
+
+        public int AverageExperience()
+        {
+            int totalExp = 0;
+
+            foreach (var person in Personnel)
+            {
+                totalExp += person.Experience;
+            }
+
+            return totalExp / Personnel.Count;
+        }
+    }
+}

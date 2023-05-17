f3d995dd23597edc4e799a1e81e960b016afd4a3
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-05-17 14:50:31 -0600

    Added spacecraft class & tests

---
+++ LaunchForce.Tests/SpaceCraftTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchForce.Tests
+{
+    public class SpaceCraftTests
+    {
+
+        [Fact]
+        public void SpaceCraft_Properties_Created()
+        {
+            SpaceCraft craft1 = new SpaceCraft("ship1", 250);
+
+            Assert.Equal("ship", craft1.Name);
+            Assert.Equal(250, craft1.Fuel);
+        }
+
+    }
+}

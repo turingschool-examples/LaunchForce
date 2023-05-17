ef3c292a712b32d1eaf6038772fa82f447f8d0ac
RafiWick <rafiwick@gmail.com>
2023-05-17 14:36:30 -0600

    Iteration 1 tests written

--- LaunchForce.Tests/PersonTests.cs
+++ LaunchForce.Tests/PersonTests.cs
@@ -1,11 +1,32 @@
+using System;
+
 namespace LaunchForce.Tests
 {
     public class PersonTests
     {
         [Fact]
-        public void Test1()
+        public void Person_Constructor_CreatesPersonObject()
         {
+            string name = "Jack White";
+            int xp = 10;
+            var testPerson = new Person(name, xp);
 
+            Assert.Equal(name, testPerson.Name);
+            Assert.Equal(xp, testPerson.Experience);
+            Assert.IsType<List<string>>(testPerson.Specialties);
+            Assert.Empty(testPerson.Specialities);
+        }
+        [Fact]
+        public void Person_AddSpecialty_AddsStringToSpecialities()
+        {
+            string name = "Jack White";
+            int xp = 10;
+            var testPerson = new Person(name, xp);
+            string speciality = "Rebounding";
+            testPerson.AddSpeciality(speciality);
+
+            Assert.Single(testPerson.Specialities);
+            Assert.Equal(speciality, testPerson.Specialities[0]);
         }
     }
 }
--- LaunchForce.Tests/SpacecraftTests.cs
+++ LaunchForce.Tests/SpacecraftTests.cs
@@ -1,11 +1,16 @@
 namespace LaunchForce.Tests
 {
-    public class PersonTests
+    public class SpacecraftTests
     {
         [Fact]
-        public void Test1()
+        public void Spececraft_Constructor_CreatesNewSpaceshipObject()
         {
+            string name = "Serenity";
+            int fuel = 400;
+            var testSpacecraft = new Spacecraft(name, fuel);
 
+            Assert.Equal(name, testSpacecraft.Name);
+            Assert.Equal(fuel, testSpacecraft.Fuel);
         }
     }
 }

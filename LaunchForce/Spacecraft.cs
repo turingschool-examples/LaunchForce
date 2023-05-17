992cf494b012c741f15175d60544493c17782f27
jcepriano <jamescepriano@gmail.com>
2023-05-17 14:12:46 -0700

    Created Spacecraft Class

---
+++ LaunchForce/Spacecraft.cs
@@ -0,0 +1,14 @@
+﻿namespace LaunchForce
+{
+    public class Spacecraft
+    {
+        public string Name;
+        public int Fuel;
+
+        public Spacecraft(string name, int fuel)
+        {
+            Name = name;
+            Fuel = fuel;
+        }
+    }
+}

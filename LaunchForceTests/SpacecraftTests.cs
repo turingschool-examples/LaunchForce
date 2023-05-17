230761c0837290500d3e4512efc35239bb32bfb2
skylarbsandler <skylarbsandler@gmail.com>
2023-05-17 14:37:22 -0600

    Added Spacecraft Class

---
+++ LaunchForceTests/SpacecraftTests.cs
@@ -0,0 +1,21 @@
+﻿using LaunchForce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchForceTests
+{
+    public class SpacecraftTests
+    {
+        [Fact]
+        public void Spacecraft_Cosntructor_CorrectlySetsPropertyValues()
+        {
+            var millenniumfalcon = new Spacecraft("Millennium Falcon", 2000);
+
+            Assert.Equal("Millennium Falcon", millenniumfalcon.Name);
+            Assert.Equal(2000, millenniumfalcon.Fuel);
+        }
+    }
+}
